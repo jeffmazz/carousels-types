389c106141e73a1f071468a64bc2b75f5e841910
jeffmazz <jeff.mazzotta@hotmail.com>
2024-10-29 23:31:00 -0300

    feat: add second corousel

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// Carousel 1
+
 const carousel = document.querySelector(".carousel_1")
 const prevBtn = document.querySelector("#previous_image_button_1")
 const nextBtn = document.querySelector('#next_image_button_1')
@@ -10,7 +12,6 @@ window.addEventListener('load', () => {
 // image width + gap
 const imageWidth = document.querySelector('.img').scrollWidth + 10
 
-
 // functions to show next and previous images
 const nextImage = () => {
     carousel.scrollBy({top: 0, left:imageWidth, behavior:"smooth"})
@@ -19,19 +20,18 @@ const prevImage = () => {
     carousel.scrollBy({top: 0, left:-imageWidth, behavior:"smooth"})
 }
 
-
-// functions to avoid multiple clicks
+// function to disable buttons and avoid multiple clicks
 const disableButtons = () => {
-    prevBtn.style.pointerEvents = "none"
-    nextBtn.style.pointerEvents = "none"
+    prevBtn.disabled = true
+    nextBtn.disabled = true
 }
-const ableButtons = () => {
+// function to enable click buttons
+const enableButtons = () => {
     setTimeout(() => {
-        prevBtn.style.pointerEvents = "auto"
-        nextBtn.style.pointerEvents = "auto"
+        prevBtn.disabled = false
+        nextBtn.disabled = false
     }, 400)
 }
-
 
 // buttons on click
 prevBtn.addEventListener("click", () => {
@@ -40,7 +40,7 @@ prevBtn.addEventListener("click", () => {
     
     disableButtons()
     prevImage()
-    ableButtons()
+    enableButtons()
 })
 nextBtn.addEventListener('click', () => {
 
@@ -48,5 +48,54 @@ nextBtn.addEventListener('click', () => {
 
     disableButtons()
     nextImage()
-    ableButtons()
+    enableButtons()
 })
+
+// Carousel 2
+
+const carousel_2 = document.querySelector('.carousel_2')
+const buttons2 = Array.from(document.querySelectorAll('.buttons_2')[0].children)
+
+const imageWidth2 = document.querySelector('.img_2').scrollWidth + 10 // scrollWidth = element width | 10 = gap
+
+// function to disable buttons and avoid multiple clicks
+const disableButtons2 = () => {
+    buttons2.forEach(button => {
+        button.disabled = true
+    })
+}
+// function to enable click buttons
+const ableButtons2 = () => {
+    setInterval(() => {
+        buttons2.forEach(button => {
+            button.disabled = false
+        })
+    }, 400)
+}
+
+// Loop to make the carousel work
+for(let index = 0; index <= 3; index ++) {
+
+    buttons2[index].addEventListener('click', () => {
+
+        buttons2.map(button => {
+            if(button === buttons2[index]) {
+                button.style.color = "rgb(36, 35, 99)"
+            } else {
+                button.style.color = "#ccc"
+            }
+        })
+
+        disableButtons2()
+
+        carousel_2.scrollTo({
+            top:0,
+            left: imageWidth2 * index,
+            behavior: 'smooth'
+        })
+
+        ableButtons2()
+
+    })
+
+}
